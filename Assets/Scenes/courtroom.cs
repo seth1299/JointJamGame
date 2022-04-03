e3b4acc601f3e44076ff445baebacaed813f7685
iankoratskydcn <44252439+iankoratskydcn@users.noreply.github.com>
2022-04-03 18:45:41 -0400

    SA

---
+++ Assets/Scenes/courtroom.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class courtroom : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static int a = 0;
+
+    public static void runCourtroom(){
+        switch (a)
+    {
+        case 0:
+
+        a = a++;
+        break;
+
+        case 1:
+
+        a = a++;
+
+        break;
+
+        case 2:
+
+        a = a++;
+        break;
+
+        case 3:
+
+        a = a++;
+        break;
+        
+        default:
+    }
+
+    }
+
+    
+}
